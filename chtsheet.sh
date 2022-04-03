4897bd31e6a207a6e347ab53d92b9d4cc482e982
Johann <johanngelhorn@gmail.com>
2022-04-03 16:43:54 +0200

    add cheatsheet script for tmux usage

---
+++ chtsheet.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# curl cht.sh/{language}/query+string
+# curl cht.sh/{core-util}~{operation}
+
+
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-utils | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
